11094d9e147626293e1444dab1a9a57d9197aa7d
NeXTzhao <1282507109@qq.com>
2022-11-12 16:43:05 +0800

    notes

--- Notes/c++_test/A.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by next on 22-4-19.
-//
-
-#include "A.h"
-inline int A::max()
-{
-    return a > b ? a : b;
-}
--- Notes/c++_test/test2/Data.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by next on 22-4-26.
-//
-
-#include "Data.h"
-
-void Data::sum() {
-std::cout << "a" << "," << "b" << std::endl;
-}
